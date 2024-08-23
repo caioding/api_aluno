7f95e1d9e9b95dc7214d9564ecc9db2966016488
caioding <7caioc@gmail.com>
2024-08-23 14:40:24 -0400

    updating

--- back/prisma/migrations/20240823183451_/migration.sql
+++ back/prisma/migrations/20240823183451_/migration.sql
@@ -5,6 +5,5 @@ CREATE TABLE `Aluno` (
     `idade` INTEGER NOT NULL,
     `matricula` VARCHAR(191) NOT NULL,
 
-    UNIQUE INDEX `Aluno_matricula_key`(`matricula`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
